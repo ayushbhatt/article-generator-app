f8fb94430557b41e910e35540fdb025a9d9bb27f
Ayush Bhatt <51471310+ayushbhatt@users.noreply.github.com>
2022-06-01 10:58:14 +0530

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'article-generator-app';
+  advice='';
+  id='';
+  shadow_class=0;
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    // this.http.get<any>('https://api.adviceslip.com/advice').subscribe(
+    // data => {
+    //       // var totalAngularPackages = data.total;
+    //       this.advice=data.slip.advice;
+    //       this.id=data.slip.id;
+    //       console.log(data.slip.advice);
+    // })
+    this.randomAdvice();
+  }
+  randomAdvice(){
+    this.http.get<any>('https://api.adviceslip.com/advice').subscribe(
+    data => {
+          // var totalAngularPackages = data.total;
+          this.advice=data.slip.advice;
+          this.id=data.slip.id;
+          console.log(data.slip.advice);
+    })
+  }
+  mouseEnterEvent(){
+    console.log("hello");
+    this.shadow_class=1;
+
+  }
+  mouseLeaveEvent(){
+    console.log("bye");
+    this.shadow_class=0;
+  }
 }
